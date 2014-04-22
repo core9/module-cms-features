407eae65e32d49bad10d435a92c08f70cccce9a8
Mark Wienk <markwienk@gmail.com>
2014-04-22 20:15:03 +0200

    moved

--- src/plugin.js
+++ src/plugin.js
@@ -2,3 +2,5 @@ angular.module( 'core9Dashboard.features', [
   'core9Dashboard.feature',
   'templates-module-cms-features'
 ]);
+
+angular.module('core9Dashboard.admin.dashboard').requires.push('core9Dashboard.features');
